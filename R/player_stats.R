81f8c3e21da8bba6b04b13829d91c7f3bd484f71
jarvis Nederlof <jarvisnederlof@gmail.com>
2020-02-29 09:21:45 -0800

    added the player stats function dn made some minor default changes to attendance.

--- R/player_stats.R
+++ R/player_stats.R
@@ -1,17 +1,21 @@
 #' player_stats
 #'
-#' Create a new factor from two existing factors, where the new factor's levels
-#' are the union of the levels of the input factors.
+#' Query the top 100 player's stats (sorted by total points)
+#' from the player's summary report endpoint on the NHL.com API.
 #'
-#' @param a factor
-#' @param b factor
+#' The stats are queried on an aggregated game-by-game basis
+#' for a range of dates. If no date is specified the function will
+#' return the player's stats for the current season. The stats to be
+#' returned are restricted to the regular season.
 #'
-#' @return factor
+#' @param a string
+#' @param b string
+#'
+#' @return data.frame
 #' @export
 #' @examples
-#' player_stats(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
-player_stats <- function(a, b) {
-  factor(c(as.character(a), as.character(b)))
+#' player_stats(start_date = "2019-10-02", end_date = "2020-02-28")
+player_stats <- function(start_date = NULL, end_date = NULL) {
 }
 
 
