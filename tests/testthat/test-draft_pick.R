c8388f12d8955773c712128d04ca4c8c1f03b208
Polina Romanchenko <romanchenko.p96@gmail.com>
2020-03-05 11:22:17 -0800

    Update draft pick

--- tests/testthat/test-draft_pick.R
+++ tests/testthat/test-draft_pick.R
@@ -1,9 +1,21 @@
-test_that("player_stats() binds factor (or character)", {
-  x <- c("a", "b")
-  x_fact <- factor(x)
-  y <- c("c", "d")
-  z <- factor(c("a", "b", "c", "d"))
-  
-  expect_identical(player_stats(x, y), z)
-  expect_identical(player_stats(x_fact, y), z)
-})
+library(testthat)
+library(rpuck)
+
+#' Tests the draft_pick function from rpuck package.
+
+test_draft_pick_r <- function(){  
+  test_that("draft_pick() function returns proper output and raises errors when expected" , {
+    #See if error is raised whith number out of range
+    expect_error(draft_pick(round_number = 45))
+    #See if error is raised with errorness input
+    expect_error(draft_pick(round_number = "nine"))
+    #See if error is raised if year out of range
+    expect_error(draft_pick(year = 45))
+    #See if error is raised whith number out of range
+    expect_error(draft_pick(pick_number = 45))
+    #See if correct length of data frame is returned 
+    expect_equal(length(draft_pick(1, 2,2010)), 1)
+    #See if error is raised if function runs into exeption
+    expect_error(draft_pick(1, 8,2010))
+  })
+}
